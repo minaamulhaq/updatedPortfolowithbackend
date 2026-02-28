95657f34b9a606b869e572e747c9a6fd3c089eff
Muhammad Inaam Ul haq <minaamulhaqoffical@gmail.com>
2026-02-28 22:51:40 +0500

    file added suucess fully

--- backend/routes/contact.route.js
+++ backend/routes/contact.route.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { creatContact, getContacts } from "../controllers/contact.model.js";
 import { protect } from "../middleware/protect.js";
 
+
 const route = Router();
 route.post("/create", creatContact);
 route.get("/all", protect, getContacts);
